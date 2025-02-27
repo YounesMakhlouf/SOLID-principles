746c1bb7fd1584dc854cfe6381c4e4e7ce58a872
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2025-02-27 21:31:28 +0100

    isp

---
+++ ISP/src/com/directi/training/isp/exercise_refactored/Sensor.java
@@ -0,0 +1,18 @@
+package com.directi.training.isp.exercise_refactored;
+
+import java.util.Random;
+
+public class Sensor {
+    public void register(ProximitySensorClient client) {
+        while (true) {
+            if (isPersonClose()) {
+                client.proximityCallback();
+                break;
+            }
+        }
+    }
+
+    private boolean isPersonClose() {
+        return new Random().nextBoolean();
+    }
+}
---
+++ ISP/src/com/directi/training/isp/exercise_refactored/Timer.java
@@ -0,0 +1,14 @@
+package com.directi.training.isp.exercise_refactored;
+
+import java.util.TimerTask;
+public class Timer {
+        public void register(long timeOut, final TimerClient client) {
+            java.util.Timer timerUtility = new java.util.Timer();
+            timerUtility.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    client.timeOutCallback();
+                }
+            }, timeOut);
+        }
+}
